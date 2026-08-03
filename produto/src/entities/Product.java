2f2a3dbe2ce438c7e25b9f337d04f12589d30592
Nicolas Gabriel <nicolasgabriel069@gmail.com>
2026-08-03 18:22:51 -0300

    estudos-java

--- produto/src/entities/Product.java
+++ produto/src/entities/Product.java
@@ -2,9 +2,9 @@ package entities;
 
 public class Product {
 	
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
